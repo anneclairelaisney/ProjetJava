10a81944875fba0b116caf9663c79fb3c275cb0d
Anne-Claire <anneclaire.laisney@hotmail.com>
2020-05-20 17:38:11 +0200

    connection + mdp + design

--- src/jdbc2020/vue/Fenetre.java
+++ src/jdbc2020/vue/Fenetre.java
@@ -16,7 +16,7 @@ import javax.swing.*;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import javafx.scene.layout.Border;
 
 /**
  *
@@ -27,113 +27,51 @@ import java.util.logging.Logger;
  */
 public class Fenetre extends JFrame implements ActionListener, ItemListener {
 
-    /*
-     * Attribut privés : objets de Connexion, AWT et Swing
-     * 
-     */
+    //Attributs
     private Connexion maconnexion;
-    private final JLabel tab, req, res, lignes;
-    private final JLabel nameBDD, requeteLabel;
-    private final JTextField requeteTexte, nameBDDTexte;
-    private final JButton exec, local;
-    private final java.awt.List listeDeTables, listeDeRequetes;
-    private final JTextArea fenetreLignes, fenetreRes;
-    private final JPanel p0, p1, nord, p2, p3;
+    private final JPanel p0, edt;
+    private CardLayout cardLayout;
 
-    /**
-     * Constructeur qui initialise tous les objets graphiques de la fenetre
-     */
-    public Fenetre() {
+    // Constructeur
+    public Fenetre(String login, String mdp, String database) throws SQLException, ClassNotFoundException {
 
         // creation par heritage de la fenetre
         super("Projet d'utilisation de JDBC dans MySQL");
+        this.maconnexion = new Connexion(database, "root", "root");
 
         // mise en page (layout) de la fenetre visible
-        this.setSize(800,600);
-	this.setTitle("INTRANET ECE PARIS-LYON");
+        this.setSize(800, 600);
+        this.setTitle("INTRANET ECE PARIS-LYON");
         this.setLayout(new BorderLayout());
-	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	this.setLocationRelativeTo(null);
-	this.setResizable(true);
-	this.setVisible(true);
-        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setResizable(true);
+        this.setVisible(true);
 
         // creation des boutons
-        //connect = new JButton("Connexion ECE");
-        local = new JButton("Connexion locale");
-        exec = new JButton("Executer");
-
         // creation des listes pour les tables et les requetes
-        listeDeTables = new java.awt.List(10, false);
-        listeDeRequetes = new java.awt.List(10, false);
-
         // creation des textes
-        nameBDDTexte = new JTextField();
-        fenetreLignes = new JTextArea();
-        fenetreRes = new JTextArea();
-        requeteTexte = new JTextField();
-
         // creation des labels
-        tab = new JLabel("Tables", JLabel.CENTER);
-        lignes = new JLabel("Lignes", JLabel.CENTER);
-        req = new JLabel("Requetes de sélection", JLabel.CENTER);
-        res = new JLabel("Résultats requête", JLabel.CENTER);
-        nameBDD = new JLabel("nom base :", JLabel.CENTER);
-        requeteLabel = new JLabel("Entrez votre requete de sélection :", JLabel.CENTER);
-
         // creation des panneaux
         p0 = new JPanel();
-        p1 = new JPanel();
-        nord = new JPanel();
-        p2 = new JPanel();
-        p3 = new JPanel();
+        edt = new JPanel();
+        javax.swing.border.Border blackline = BorderFactory.createLineBorder(Color.black, 1);
+        for (int i = 0; i < 65; i++) {
+            JPanel ptest = new JPanel();
+            ptest.setBorder(blackline);
+            edt.add(ptest);
+        }
 
         // mise en page des panneaux
         p0.setLayout(new GridLayout(1, 3));
-        p1.setLayout(new GridLayout(1, 4));
-        nord.setLayout(new GridLayout(2, 1));
-        p2.setLayout(new GridLayout(1, 4));
-        p3.setLayout(new GridLayout(1, 3));
+        edt.setLayout(new GridLayout(13, 5));
 
-        // ajout des objets graphqiues dans les panneaux
-        p0.add(nameBDD);
-        p0.add(nameBDDTexte);
-        p0.add(local);
-        p1.add(tab);
-        p1.add(lignes);
-        p1.add(req);
-        p1.add(res);
-        nord.add("North", p0);
-        nord.add("North", p1);
-        p2.add(listeDeTables);
-        p2.add(fenetreLignes);
-        p2.add(listeDeRequetes);
-        p2.add(fenetreRes);
-        p3.add(requeteLabel);
-        p3.add(requeteTexte);
-        p3.add(exec);
-
+        this.add(p0);
+        this.add(edt);
+        // ajout des objets graphiques dans les panneaux
         // ajout des listeners
-        exec.addActionListener(this);
-        local.addActionListener(this);
-        listeDeTables.addItemListener(this);
-        listeDeRequetes.addItemListener(this);
-
         // couleurs des objets graphiques
-        tab.setBackground(Color.MAGENTA);
-        lignes.setBackground(Color.MAGENTA);
-        req.setBackground(Color.MAGENTA);
-        res.setBackground(Color.MAGENTA);
-        listeDeTables.setBackground(Color.CYAN);
-        fenetreLignes.setBackground(Color.WHITE);
-        listeDeRequetes.setBackground(Color.GREEN);
-        fenetreRes.setBackground(Color.WHITE);
-        p1.setBackground(Color.LIGHT_GRAY);
-
         // disposition geographique des panneaux
-        add("North", nord);
-        add("Center", p2);
-        add("South", p3);
 
         // pour fermer la fenetre
         addWindowListener(new WindowAdapter() {
@@ -144,9 +82,7 @@ public class Fenetre extends JFrame implements ActionListener, ItemListener {
         });
     }
 
-    /**
-     * Méthode privée qui initialise la liste des tables
-     */
+    // Initialisation des tables
     private void remplirTables() {
         maconnexion.ajouterTable("COURS");
         maconnexion.ajouterTable("ENSEIGNANT");
@@ -164,220 +100,18 @@ public class Fenetre extends JFrame implements ActionListener, ItemListener {
 
     }
 
-    /**
-     * Méthode privée qui initialise la liste des requetes de selection
-     */
-    private void remplirRequetes() {
-    }
-
-    /**
-     * Méthode privée qui initialise la liste des requetes de MAJ
-     */
-    private void remplirRequetesMaj() throws SQLException {
-        // Requêtes d'insertion
-
-        // Requêtes de modification
-
-        // Requêtes de suppression
-    }
-
-    /**
-     *
-     * Afficher les tables
-     */
-    public void afficherTables() {
-        for (String table : maconnexion.tables) {
-            listeDeTables.add(table);
-        }
-    }
-
-    /**
-     *
-     * Afficher les lignes de la table sélectionnée
-     *
-     * @param nomTable
-     */
-    public void afficherLignes(String nomTable) {
-        try {
-            ArrayList<String> liste;
-
-            // effacer les résultats
-            fenetreLignes.removeAll();
-
-            // recupérér les résultats de la table selectionnee
-            liste = maconnexion.remplirChampsTable(nomTable);
-
-            // afficher les champs de la table selectionnee 
-            fenetreLignes.setText("");
-            for (String liste1 : liste) {
-                fenetreLignes.append(liste1);
-            }
-
-            // recuperer la liste de la table sélectionnée
-            String requeteSelectionnee = "select * from " + nomTable + ";";
-            liste = maconnexion.remplirChampsRequete(requeteSelectionnee);
-
-            // afficher les lignes de la requete selectionnee a partir de la liste
-            for (String liste1 : liste) {
-                fenetreLignes.append(liste1);
-            }
-
-        } catch (SQLException e) {
-            // afficher l'erreur dans les résultats
-            fenetreRes.setText("");
-            fenetreRes.append("Echec table SQL");
-            e.printStackTrace();
-
-        }
-    }
-
-    /**
-     *
-     * Afficher les requetes de selection et de MAJ dans la fenetre
-     */
-    public void afficherRequetes() throws SQLException {
-        for (String requete : maconnexion.requetes) {
-            listeDeRequetes.add(requete);
-        }
-
-        for (String requete : maconnexion.requetesMaj) {
-            listeDeRequetes.add(requete);
-            maconnexion.executeUpdate(requete);
-        }
-    }
-
-    /**
-     *
-     * Afficher et retourner les résultats de la requete sélectionnée
-     *
-     * @param requeteSelectionnee
-     * @return
-     * @throws java.sql.SQLException
-     */
     public ArrayList<String> afficherRes(String requeteSelectionnee) throws SQLException {
         ArrayList<String> liste = null;
-        try {
-
-            // effacer les résultats
-            fenetreRes.removeAll();
-
-            // recupérér les résultats de la requete selectionnee
-            liste = maconnexion.remplirChampsRequete(requeteSelectionnee);
-
-            // afficher les lignes de la requete selectionnee a partir de la liste
-            fenetreRes.setText("");
-            for (String liste1 : liste) {
-                fenetreRes.append(liste1);
-            }
-        } catch (SQLException e) {
-            // afficher l'erreur dans les résultats
-            fenetreRes.setText("");
-            fenetreRes.append("Echec requete SQL");
-        }
         return liste;
     }
 
-    /**
-     *
-     * Pour gerer les actions sur les boutons on utilise la fonction
-     * actionPerformed
-     *
-     * @param evt
-     */
     @Override
     @SuppressWarnings("CallToThreadDumpStack")
     public void actionPerformed(ActionEvent evt) {
-        Object source = evt.getSource();
-
-        // tester cas de la commande evenementielle
-        if (source == local) {
-            ArrayList<String> liste;
-            try {
-                try {
-                    // tentative de connexion si les 4 attributs sont remplis
-                    //maconnexion = new Connexion("jps", "root", "");
-                    maconnexion = new Connexion(nameBDDTexte.getText(), "root", "root");
-
-                    // effacer les listes de tables et de requêtes
-                    listeDeTables.removeAll();
-                    listeDeRequetes.removeAll();
-
-                    // initialisation de la liste des requetes de selection et de MAJ
-                    remplirTables();
-                    remplirRequetes();
-                    remplirRequetesMaj();
-
-                    // afficher la liste de tables et des requetes
-                    afficherTables();
-                    afficherRequetes();
-
-                    // se positionner sur la première table et requête de selection
-                    listeDeTables.select(0);
-                    listeDeRequetes.select(0);
-
-                    // afficher les champs de la table sélectionnée
-                    String nomTable = listeDeTables.getSelectedItem();
-
-                    // recuperer les lignes de la table selectionnee
-                    afficherLignes(nomTable);
-
-                    // recuperer la liste des lignes de la requete selectionnee
-                    String requeteSelectionnee = listeDeRequetes.getSelectedItem();
-
-                    // afficher les résultats de la requete selectionnee
-                    afficherRes(requeteSelectionnee);
-                } catch (ClassNotFoundException cnfe) {
-                    System.out.println("Connexion echouee : probleme de classe");
-                    cnfe.printStackTrace();
-                }
-            } catch (SQLException e) {
-                System.out.println("Connexion echouee : probleme SQL");
-                e.printStackTrace();
-            }
-        } else if (source == exec) {
-            String requeteSelectionnee = requeteTexte.getText(); // récupérer le texte de la requête
-
-            // effacer les résultats
-            fenetreRes.removeAll();
-
-            try {
-                // afficher les résultats de la requete selectionnee
-                if (afficherRes(requeteSelectionnee) != null) {
-                    maconnexion.ajouterRequete(requeteSelectionnee);
-                    listeDeRequetes.removeAll();
-                    afficherRequetes();
-                }
-
-            } catch (SQLException ex) {
-
-            }
-
-        }
     }
 
-    /**
-     *
-     * Pour gerer les actions sur items d'une liste on utilise la methode
-     * itemStateChanged
-     *
-     * @param evt
-     */
     @Override
-    @SuppressWarnings("CallToThreadDumpStack")
-    public void itemStateChanged(ItemEvent evt) {
-        // sélection d'une requete et afficher ses résultats
-        if (evt.getSource() == listeDeRequetes) {
-            // recuperer la liste des lignes de la requete selectionnee
-            String requeteSelectionnee = listeDeRequetes.getSelectedItem();
-            try {
-                afficherRes(requeteSelectionnee);
-            } catch (SQLException ex) {
-                Logger.getLogger(Fenetre.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } else if (evt.getSource() == listeDeTables) {
-            // afficher les lignes de la table sélectionnée
-            String nomTable = listeDeTables.getSelectedItem();
-            afficherLignes(nomTable);
-        }
+    public void itemStateChanged(ItemEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
